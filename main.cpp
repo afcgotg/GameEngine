b1ca220e6d7df2406161a2dbb6d66dd7d10c5692
Alex Fernandez <fernialex@gmail.com>
2023-02-04 19:49:57 +0100

    pg 16

--- main.cpp
+++ main.cpp
@@ -3,29 +3,45 @@
 
 const int WIDTH = 800, HEIGHT = 600;
 
-int main(int argc, char *argv[]){
-    SDL_Init(SDL_INIT_EVERYTHING);
+SDL_Window* g_pWindow = 0;
+SDL_Renderer* g_pRenderer = 0;
 
-    SDL_Window *window = SDL_CreateWindow("Hello SDL world!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_ALLOW_HIGHDPI);
+int main(int argc, char* argv[]){
+   
+   if(SDL_Init(SDL_INIT_EVERYTHING) >= 0){
+    // if succeeded create a window
+    g_pWindow = SDL_CreateWindow("Chapter 1: Settingup SDL",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        WIDTH, HEIGHT,
+        SDL_WINDOW_SHOWN);
 
-    if( window == NULL){
-        std::cout << "No se ha podido crear la ventana: " << SDL_GetError() << std::endl;
-        return 1;
+    // if the window creation succeeded create our render
+    if(g_pWindow != 0){
+        g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+    }else{
+        std::cout << "Error when initialize the window: " << SDL_GetError() << std::endl;
     }
+   }else{
+    return 1; //SDL could not initialize
+   }
 
-    SDL_Event windowEvent;
+   // everything succeeded lets draw the window
 
-    while(true){
-        if(SDL_PollEvent( &windowEvent)){
-            if(SDL_QUIT == windowEvent.type){
-                break;
-            }
-        }
-    }
+   // set to black
+   // this function expects Red, Green, Blue and Alpha as colour values
+   SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
 
-    SDL_DestroyWindow(window);
-    SDL_Quit();
+   // clear the window to black
+   SDL_RenderClear(g_pRenderer);
+   
+   // show the window
+   SDL_RenderPresent(g_pRenderer);
 
-    return EXIT_SUCCESS;
+   //set a delay before quitting
+   SDL_Delay(5000);
 
+   // clean up SDL
+   SDL_Quit();
+
+   return 0;
 }
